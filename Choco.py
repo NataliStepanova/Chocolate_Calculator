d588d76b04f404837a006cd659ea5a1aa3976eee
Nata <angaraido@gmail.com>
2023-04-04 15:21:09 +0300

    Подключено сохранение в PDF

--- Choco.py
+++ Choco.py
@@ -1,3 +1,5 @@
+import jinja2
+import pdfkit
 gr_vsego = int(input('Укажите вес готового продукта(гр) '))
 proc_kakao = int(input('Укажите % какао (гр) '))
 proc_maslo = int(input('Укажите % какао-масла (гр) '))
@@ -13,6 +15,28 @@ gr_pudra = gr_vsego - (gr_kakao + gr_maslo)
 fat_gr_kakao = gr_kakao / KAKAO_FAT_PERCENT
 # кол-во масла (gr_maslo) это и есть 100% жира
 fat_gr_vsego = fat_gr_kakao + gr_maslo
-print('Для приготовления ' + str(gr_vsego) + ' ' + 'грамм шоколада понадобится: \n' + 'Вес какао-бобов ' + str(gr_kakao) + ' \n' +
+print('Для приготовления ' + str(gr_vsego) + ' грамм шоколада понадобится: \n' + 'Вес какао-бобов ' + str(gr_kakao) + ' \n' +
       'Вес какао-масла ' + str(gr_maslo) + ' \n' + 'Вес сахарной пудры ' + str(gr_pudra))
+# массовую долю жира ограничить 2мя знаками
 print('Массовая доля жира: ' + str(fat_gr_vsego))
+
+data_for_template = {
+    'gr_vsego': str(gr_vsego),
+    'gr_kakao': str(gr_kakao),
+    'gr_maslo': str(gr_maslo),
+    'gr_pudra': str(gr_pudra),
+    'fat_gr_vsego': str(fat_gr_vsego)
+}
+
+template_loader = jinja2.FileSystemLoader('./')
+
+template_env = jinja2.Environment(loader=template_loader)
+
+template = template_env.get_template('Template.html')
+
+output_text = template.render(data_for_template)
+
+config = pdfkit.configuration(
+    wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
+pdfkit.from_string(
+    output_text, './recipes/pdf_generated.pdf', configuration=config)
